259e05ce139865a1ae97a2e453720627917efee2
jeremyfelt <jeremy.felt@gmail.com>
2014-10-20 12:29:46 -0700

    Remove previous filtering of title and nav label for overview

--- functions.php
+++ functions.php
@@ -62,29 +62,6 @@ function cob_bu_navigation_filter_item_attrs( $item_classes, $page ) {
 	return $item_classes;
 }
 
-add_filter( 'bu_navigation_filter_anchor_attrs', 'cob_bu_navigation_filter_anchor_attrs', 10, 2 );
-/**
- * Filter anchor attributes in generate page menu to remove the title so that our default Overview
- * behavior remains.
- *
- * If a label is assigned to the page, it will be displayed as the section label. The actual page
- * title will be used as the Overview replacement.
- *
- * @param array   $attrs List of attributes to output as part of the anchor.
- * @param WP_Post $page  Post object for the current page.
- *
- * @return mixed
- */
-function cob_bu_navigation_filter_anchor_attrs( $attrs, $page ) {
-	if ( trim( $page->post_title ) !== $attrs['title'] ) {
-		$attrs['title'] = $page->post_title;
-	} else {
-		$attrs['title'] = '';
-	}
-
-	return $attrs;
-}
-
 add_filter( 'wsuwp_content_syndicate_json', 'cob_content_syndication_display', 10, 2 );
 /**
  * Add a container to the output of the JSON data.
