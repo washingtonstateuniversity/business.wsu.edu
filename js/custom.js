f390e1b7c45e165076c88251d4138cd7cf16ed73
jeremyfelt <jeremy.felt@gmail.com>
2014-10-21 13:24:37 -0700

    Remove additional is_home() function, as we only use it once

--- js/custom.js
+++ js/custom.js
@@ -2,28 +2,14 @@
 FontDetect=function(){function e(){if(!n){n=!0;var e=document.body,t=document.body.firstChild,i=document.createElement("div");i.id="fontdetectHelper",r=document.createElement("span"),r.innerText="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",i.appendChild(r),e.insertBefore(i,t),i.style.position="absolute",i.style.visibility="hidden",i.style.top="-200px",i.style.left="-100000px",i.style.width="100000px",i.style.height="200px",i.style.fontSize="100px"}}function t(e,t){return e instanceof Element?window.getComputedStyle(e).getPropertyValue(t):window.jQuery?$(e).css(t):""}var n=!1,i=["serif","sans-serif","monospace","cursive","fantasy"],r=null;return{onFontLoaded:function(t,i,r,o){if(t){var s=o&&o.msInterval?o.msInterval:100,a=o&&o.msTimeout?o.msTimeout:2e3;if(i||r){if(n||e(),this.isFontLoaded(t))return void(i&&i(t));var l=this,f=(new Date).getTime(),d=setInterval(function(){if(l.isFontLoaded(t))return clearInterval(d),void i(t);var e=(new Date).getTime();e-f>a&&(clearInterval(d),r&&r(t))},s)}}},isFontLoaded:function(t){var o=0,s=0;n||e();for(var a=0;a<i.length;++a){if(r.style.fontFamily='"'+t+'",'+i[a],o=r.offsetWidth,a>0&&o!=s)return!1;s=o}return!0},whichFont:function(e){for(var n=t(e,"font-family"),r=n.split(","),o=r.shift();o;){o=o.replace(/^\s*['"]?\s*([^'"]*)\s*['"]?\s*$/,"$1");for(var s=0;s<i.length;s++)if(o==i[s])return o;if(this.isFontLoaded(o))return o;o=r.shift()}return null}}}();
 
 (function($, FontDetect, window){
-	var view_data = {
-		is_home : 0
-	};
+
+	var view_data = {};
 
 	var incrementor = {
 		background: 4.5,
 		margin: 6.25,
 		opacity: 500,
 		cta_opacity: 200
-	};
-
-	/**
-	 * Determine if this page view has the `home` class assigned to body.
-	 *
-	 * @returns bool True if home page, false if not.
-	 */
-	var is_home = function() {
-		if ( 0 === view_data.is_home ) {
-			view_data.is_home = ( $('.home' ).length > 0 );
-		}
-
-		return view_data.is_home;
 	};
 
 	var is_ios = function() {
@@ -88,7 +74,7 @@ FontDetect=function(){function e(){if(!n){n=!0;var e=document.body,t=document.bo
 	};
 
 	$(document).ready(function(){
-		if ( ! is_home() && ! is_ios() && 0 !== $('.has-background-image' ).length ) {
+		if ( ! is_ios() && 0 === $('.home' ).length && 0 !== $('.has-background-image' ).length ) {
 			FontDetect.onFontLoaded( 'Open Sans Condensed', on_font_loaded, function() { console.log('did not load' ) }, {msTimeout: 3000});
 		}
 	});
