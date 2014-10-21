c1e5c241cd5bf5aab066ec683952206653844a24
jeremyfelt <jeremy.felt@gmail.com>
2014-10-20 17:08:31 -0700

    Refactor handling of parallax effects on secondary pages

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,46 @@
 (function($, window){
 	if ( 0 === $('.home' ).length ) {
 		// Thanks to a good write-up on approach from https://medium.com/@TonyJing/medium-style-header-aa738696c6ac
-		var scroll_top = $(window).scrollTop();
-		var cob_headline_container = $('.cob-headline-container');
+		var last_position = $(window).scrollTop();
 
-		// Background images are 792px high, calculate our headroom.
-		var head_room = 792 - $('.main-header' ).height();
+		var cob_headline_container = $('.cob-headline-container');
+		var cob_headline_container_height = cob_headline_container.height();
+		var main_header = $('.main-header');
+		var main_header_height = main_header.height();
+
+		var page_cta = $('.page-call-to-action');
+
+		//rmain_header.css( { 'height' : main_header_height } );
 
 		// The headline container starts with no margin property.
 		var margin_top = 0;
+		var header_opacity = 1;
+		var cta_opacity = 1;
 
 		$(window).on( 'scroll', function() {
-			var scroll_amount = $( this ).scrollTop();
-			var delta = scroll_amount - scroll_top;
+			var page_position = $( this ).scrollTop();
 
-			margin_top += ( delta / 4.5 );
-
-			cob_headline_container.css( { 'margin-top' : margin_top, 'opacity' : "-=" + delta / 700 } );
-			scroll_top = scroll_amount;
-
-			// If we manage to scroll with momentum, make sure opacity stays sane.
-			if ( 0 > scroll_amount ) {
-				cob_headline_container.css( 'opacity', '1' );
+			if ( page_position > last_position ) {
+				// scrolling down
+				main_header_height = main_header_height - ( page_position - last_position );
+				margin_top = margin_top - ( ( page_position - last_position ) / 1.85 );
+				header_opacity = header_opacity - ( ( page_position - last_position ) / 300 );
+				cta_opacity = cta_opacity - ( ( page_position - last_position ) / 200 );
+			} else {
+				// scrolling up
+				main_header_height = main_header_height + ( last_position - page_position );
+				margin_top = margin_top + ( ( last_position - page_position ) / 1.85 );
+				header_opacity = header_opacity + ( ( last_position - page_position ) / 300 );
+				cta_opacity = cta_opacity + ( ( last_position - page_position ) / 200 );
 			}
+			last_position = page_position;
 
-			// If the background image, at 792px hits the bottom of the main header container, stop margining.
-			if ( head_room < margin_top ) {
-				cob_headline_container.css( 'margin-top', head_room );
+			if ( main_header_height < ( cob_headline_container_height - 200 ) ) {
+				// Avoid overflow.
+			} else {
+				main_header.css( { 'height' : main_header_height } );
+				cob_headline_container.css( { 'margin-top' : margin_top, 'opacity' : header_opacity } );
+				page_cta.css( { 'opacity' : cta_opacity } );
 			}
 		});
 	}
